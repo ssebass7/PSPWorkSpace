83a46d2adf1c489b0343921fb16109cf2b97608a
Sebastián <ssebass2000@gmail.com>
2021-03-09 19:07:30 +0100

    Ejercicio criptografia

---
+++ 28_Criptografia/src/_02_asimetrica/_03_MainRSA.java
@@ -0,0 +1,67 @@
+package _02_asimetrica;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+import javax.crypto.Cipher;
+
+/*
+ * En este apartado estudiaremos el algoritmo RSA, 
+ * Desarrollado por Ron Rivest, Adi Shamir y Leonard Adleman 
+ * cuyas iniciales dieron nombre al algoritmo. 
+ * 
+ *  La clave privada, que es distinta para cada usuario. 
+
+    La clave pública, que es la misma para todos los usuarios.
+
+	El procedimiento empleado para el cifrado es el siguiente:
+
+    Un proceso que desea enviar un mensaje utiliza la clave pública 
+    (conocida por todos los procesos) para cifrar el mensaje.
+
+    El proceso destinatario del mensaje podrá descifrarlo utilizando la 
+    clave privada, que solo él conoce, asegurando así la confidencialidad.
+
+    La clave privada es generada a partir de la clave pública, 
+    de modo que ambas están relacionadas.
+ */
+public class _03_MainRSA {
+	public static void main(String[] args) {
+		try {
+			//En vez de KeyGenerator usamos KeyPairGenerator
+			KeyPairGenerator generador = KeyPairGenerator.getInstance("RSA");
+			System.out.println("Paso 1: Se ha obtenido el generador de claves");
+			
+			//Obtenemos el par de claves (publica y privada)
+			KeyPair claves = generador.generateKeyPair();
+			System.out.println("Paso 2: Se ha obtenido el par de claves");
+			
+			Cipher cifrador = Cipher.getInstance("RSA");
+			System.out.println("Paso 3: Hemos obtenido el descifrador");
+			
+			cifrador.init(Cipher.ENCRYPT_MODE, claves.getPublic());
+			System.out.println(claves.getPublic().getClass().getName());
+			System.out.println("Paso 4: Hemos configurado el descifrador para usar clave publica");
+			
+			String mensajeOriginal = "La cripta mágica";
+			byte[] bytesMensajeOriginal = mensajeOriginal.getBytes();
+			//ciframos el mensaje
+			byte[] bytesMensajeCifrado = cifrador.doFinal(bytesMensajeOriginal);
+			String mensajeCifrado = new String(bytesMensajeCifrado);
+			System.out.println("Paso 5: Hemos preparado y cifrado el mensaje original");
+			System.out.println("Mensaje Original: " + mensajeOriginal);
+			System.out.println("Mensaje Cifrado: " + mensajeCifrado);
+			
+			System.out.println("AHORA VAMOS A DESCIFRAR EL MENSAJE CIFRADO USANDO LA CLAVE PRIVADA");
+			cifrador.init(Cipher.DECRYPT_MODE, claves.getPrivate());
+			byte[] bytesMensajeDescifrado = cifrador.doFinal(bytesMensajeCifrado);
+			System.out.println("Mensaje Descifrado: " + new String(bytesMensajeDescifrado));
+			
+		} catch (GeneralSecurityException e) {
+			System.out.println("Error al cifrar o descifrar el mensaje");
+			System.out.println("Excepción de tipo: " + e.getClass().getName());
+			System.out.println(e.getMessage());
+		}
+	}
+}
---
+++ 28_Criptografia/src/_04_hash/Resumen.java
@@ -0,0 +1,47 @@
+package _04_hash;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Resumen {
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		byte[] cancion1 = "Bohemian Rhapsody".getBytes();
+		byte[] cancion2 = "Imagine".getBytes();
+		byte[] cancion3 = "We Are the Champions".getBytes();
+		byte[] cancion4 = "Hotel California".getBytes();
+		byte[] cancion5 = "A ella le gusta la gasolina".getBytes();
+		
+		/*
+		 * Creamos un objeto MessageDigest a través del método estático 
+		 * getInstance() al que se le pasa el tipo de algoritmo que vamos a 
+		 * utilizar. En este caso, el algoritmo MD5. Luego, actualizamos 
+		 * nuestro objeto con todos los mensajes que queremos unir para 
+		 * crear el resumen hash.
+		 */
+		//MessageDigest md = MessageDigest.getInstance("SHA-512");
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		md.update(cancion1);
+		md.update(cancion2);
+		md.update(cancion3);
+		md.update(cancion4);
+		md.update(cancion5);
+		
+		//El último paso será ejecutar el método digest() de nuestro 
+		//objeto MessageDigest para obtener el resumen, que también 
+		//será una cadena de bytes.
+		
+		byte[] resumen = md.digest();
+		String mensaje = new String(resumen);
+		System.out.println("Resumen hash: " + mensaje);
+		
+		//Mejor pasarlo a Base64
+		String mensajeHashBase64 = Base64.getEncoder().encodeToString(resumen);
+		System.out.println(mensajeHashBase64);
+		
+		//O a Hexadecimal
+		String mensajeHashHex = javax.xml.bind.DatatypeConverter.printHexBinary(resumen);
+		System.out.println(mensajeHashHex);
+	}
+}
