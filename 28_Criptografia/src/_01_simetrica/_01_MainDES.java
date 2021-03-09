e54c670c827f75f32ce560cad2f4fe7c087808a5
Sebastián <ssebass2000@gmail.com>
2021-03-09 19:13:48 +0100

    El bueno es el otro proyecto. Esto solo son ejemplos

--- 28_Criptografia/src/_01_simetrica/_01_MainDES.java
+++ 28_Criptografia/src/_01_simetrica/_01_MainDES.java
@@ -11,7 +11,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-
+/*EJEMPLO*/
 /*
  * características del algoritmo DES.
 
